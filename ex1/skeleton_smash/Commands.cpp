d383f4afb08e12cba2b09dc3a50c28369a695a1b
Batel Oved <bateloved1@gmail.com>
2022-04-17 13:02:23 +0000

    Add commands to shell

--- ex1/skeleton_smash/Commands.cpp
+++ ex1/skeleton_smash/Commands.cpp
@@ -82,12 +82,20 @@ void _removeBackgroundSign(char* cmd_line) {
 
 // TODO: Add your implementation for classes in Commands.h 
 
-SmallShell::SmallShell() {
-// TODO: add your implementation
+SmallShell::SmallShell(): curr_dir((char*)malloc(COMMAND_ARGS_MAX_LENGTH)), prev_dir(nullptr) {
+  getcwd(this->curr_dir, COMMAND_ARGS_MAX_LENGTH);
 }
 
 SmallShell::~SmallShell() {
-// TODO: add your implementation
+  free(this->curr_dir);
+}
+
+JobsList::JobsList() {
+
+}
+
+JobsList::~JobsList() {
+  
 }
 
 /**
@@ -95,9 +103,10 @@ SmallShell::~SmallShell() {
 */
 Command * SmallShell::CreateCommand(const char* cmd_line) {
 	// For example:
-/*
+  char** args = (char**)malloc(COMMAND_ARGS_MAX_LENGTH);
   string cmd_s = _trim(string(cmd_line));
   string firstWord = cmd_s.substr(0, cmd_s.find_first_of(" \n"));
+  _parseCommandLine(cmd_line, args);
 
   if (firstWord.compare("pwd") == 0) {
     return new GetCurrDirCommand(cmd_line);
@@ -105,19 +114,123 @@ Command * SmallShell::CreateCommand(const char* cmd_line) {
   else if (firstWord.compare("showpid") == 0) {
     return new ShowPidCommand(cmd_line);
   }
-  else if ...
-  .....
+  else if (firstWord.compare("cd") == 0) {
+    return new ChangeDirCommand(cmd_line, &this->prev_dir);
+  }
+  else if (firstWord.compare("chprompt") == 0) {
+    return new ChangePrompt(cmd_line, args[1]);
+  }
+  
   else {
     return new ExternalCommand(cmd_line);
   }
-  */
+  free(args);
   return nullptr;
 }
 
 void SmallShell::executeCommand(const char *cmd_line) {
   // TODO: Add your implementation here
   // for example:
-  // Command* cmd = CreateCommand(cmd_line);
-  // cmd->execute();
+  Command* cmd = CreateCommand(cmd_line);
+  cmd->execute();
   // Please note that you must fork smash process for some commands (e.g., external commands....)
 }
+
+Command::Command(const char* cmd_line) {
+
+}
+
+Command::~Command() {
+
+}
+
+BuiltInCommand::BuiltInCommand(const char* cmd_line): Command(cmd_line) {
+
+}
+
+ExternalCommand::ExternalCommand(const char* cmd_line): Command(cmd_line) {
+
+}
+
+void ExternalCommand::execute() {
+  
+}
+
+ChangePrompt::ChangePrompt(const char* cmd_line, const char* prompt_name): BuiltInCommand(cmd_line) {
+  SmallShell& smash = SmallShell::getInstance();
+
+  if (name) {
+    this->name = (char*)malloc(sizeof(name)+1);
+    strcpy(this->name, name);
+  }
+  else
+    this->name = (char*)malloc(sizeof("smash")+1);
+    strcpy(this->name, "smash");
+}
+
+void ChangePrompt::execute() {
+    // while(true) {
+    //     std::cout << this->name << "> ";
+    //     std::string cmd_line;
+    //     std::getline(std::cin, cmd_line);
+    //     smash.executeCommand(cmd_line.c_str());
+    // }
+}
+
+ChangeDirCommand::ChangeDirCommand(const char* cmd_line, char** plastPwd): BuiltInCommand(cmd_line), 
+            curr_dir((char*)malloc(COMMAND_ARGS_MAX_LENGTH)), prev_dir((char*)malloc(COMMAND_ARGS_MAX_LENGTH)), 
+                                                                            path((char*)malloc(COMMAND_ARGS_MAX_LENGTH)) {
+  char** args = (char**)malloc(COMMAND_ARGS_MAX_LENGTH);
+  string cmd_s = _trim(string(cmd_line));
+  _parseCommandLine(cmd_line, args);
+
+  if (args[2])
+    perror("smash error: cd: too many arguments");
+
+  if (strcmp(args[1], "-") == 0 && !plastPwd)
+    perror("smash error: cd: OLDPWD not set");
+
+  strcpy(this->path, args[1]);
+}
+
+void ChangeDirCommand::execute() { //TODO: Add error handling
+  if (strcmp(this->path, "-") == 0) {
+    chdir(this->prev_dir);
+    char* temp = this->prev_dir;
+    this->prev_dir = this->curr_dir;
+    this->curr_dir = temp;
+
+
+    char* temp = this->prev_dir;
+    getcwd(this->curr_dir, COMMAND_ARGS_MAX_LENGTH);
+    chdir(this->prev_dir);
+    getcwd(this->prev_dir, COMMAND_ARGS_MAX_LENGTH);
+  }
+  else if (strcmp(this->path, "..") == 0) {
+    
+  }
+  else {
+    getcwd(this->prev_dir, COMMAND_ARGS_MAX_LENGTH);
+    chdir(this->path);
+    getcwd(this->curr_dir, COMMAND_ARGS_MAX_LENGTH);
+  }
+}
+
+GetCurrDirCommand::GetCurrDirCommand(const char* cmd_line): BuiltInCommand(cmd_line) {
+  
+}
+
+void GetCurrDirCommand::execute() {
+  char* buff = (char*)malloc(COMMAND_ARGS_MAX_LENGTH);
+  cout << getcwd(buff, COMMAND_ARGS_MAX_LENGTH) << endl;
+  free(buff);
+}
+
+ShowPidCommand::ShowPidCommand(const char* cmd_line): BuiltInCommand(cmd_line) {
+
+}
+
+void ShowPidCommand::execute() {
+  cout << "smash pid is " << getpid() << endl;
+}
+
